29f9c81ff8ce4eb7ee19a99bb4918931172e28ac
Juspher Mokua <juspermokua6@gmail.com>
2022-10-06 00:01:02 +0300

    done assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,19 @@
 import React from "react";
-import { directors } from "../data";
+import { directors, movies } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const list =  directors.map((director) => 
+  <div key={director.name}>
+    <h3>Name: {director.name}</h3>
+    <h3>Movies:</h3>
+      <ul>
+        {director.movies.map((movie) => <li key={movie}>{movie}</li>)}
+      </ul>
+  </div>)
+return <div>
+  <h1>Directors Page</h1>
+  {list}
+</div>;
 }
 
 export default Directors;
